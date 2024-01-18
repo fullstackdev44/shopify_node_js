72a6ecd7288ccbff0d0d02574e2c5ec4595439d5
fullstackdev44 <fullstackdeveloper44@gmail.com>
2024-01-18 17:22:07 +0530

    file

--- index.js
+++ index.js
@@ -1,29 +1,107 @@
 const express = require('express');
+const multer = require('multer');
+const AWS = require('aws-sdk');
+const cors = require('cors'); // Import the cors middleware
 const app = express();
-const { exec } = require('child_process');
-const port = 4000; // You can change the port if needed
+const port = process.env.PORT || 4000;
+// Set up multer to handle file uploads
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
 
-app.get('/', (req, res) => {
-  res.send('Hello, World!'); // Sends 'Hello, World!' as the response
+app.use(cors());
+const credentials = new AWS.Credentials({
+  accessKeyId: 'AKIAR2DOH4FYHHY3XTDE', // Replace with your AWS_ACCESS_KEY
+  secretAccessKey: 'DVDxChed1uTMfGndj7OIKY0s5HaeuzmH0y8cgI1z', // Replace with your AWS_SECRET_KEY
 });
-app.get('/hello', (req, res) => {
- // Function to shut down the computer
-function shutdownComputer() {
-  // Use the appropriate command based on the operating system
-  const command = process.platform === 'win32' ? 'shutdown /s /t 0' : 'shutdown now';
 
-  exec(command, (error, stdout, stderr) => {
-    if (error) {
-      console.error(`Error shutting down: ${error.message}`);
-    } else {
-      console.log('Computer is shutting down...');
+const accessKeyId = 'AKIAR2DOH4FYIEYX374P';
+const secretAccessKey = 'ezMiaN7Fqj1154Rf+DVsVeAwdovU2iHZHy6/Bzta';
+AWS.config.update({
+  accessKeyId: accessKeyId,
+  secretAccessKey: secretAccessKey,
+  region: 'us-east-1', // Replace with your AWS_REGION
+});
+
+const s3 = new AWS.S3();
+
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
+
+const lambdaFunctions = [
+  'lambda_instance_1',
+  'lambda_instance_2',
+  'lambda_instance_3',
+  'lambda_instance_4',
+  'lambda_instance_5',
+  'lambda_instance_6'
+];
+app.post('/upload', async (req, res) => {
+  try {
+    const base64Image = req.body.file;
+    if (!base64Image) {
+      throw new Error('Image data is missing in the request body');
     }
-  });
-}
-shutdownComputer();
-});
-app.get('/', (req, res) => {
-  res.send('Hello, World!'); // Sends 'Hello, World!' as the response
+    const current_date = new Date();
+    const formatted_date = current_date.toLocaleDateString('en-US', { month: '2-digit', day: '2-digit', year: 'numeric' }).replace(/\//g, '-');
+    const bucket_name = 'pp-unprocessed-images';
+
+    const base64_image = base64Image;
+    const image_data = Buffer.from(base64_image, 'base64');
+
+    const key = `${formatted_date}/${Math.floor(Math.random() * (99999 - 10000 + 1)) + 10000}-${Math.floor(new Date() / 1000)}.jpg`;
+
+    await s3.putObject({
+      Body: image_data,
+      Bucket: bucket_name,
+      Key: key,
+      ContentType: 'image/jpeg'
+    }).promise();
+
+    const object_url = `https://${bucket_name}.s3.amazonaws.com/${key}`;
+    const response = {
+      statusCode: 200,
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': 'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token',
+        'Access-Control-Allow-Methods': 'POST,OPTIONS',
+      },
+      body: { success: true, objectUrl: object_url }
+    };
+
+    AWS.config.update({
+      credentials,
+      region: 'us-east-1', // Replace with your AWS_REGION
+    });
+
+    const lambda = new AWS.Lambda();
+    const aggregatedData = [];
+
+    for (const functionName of lambdaFunctions) {
+      const payload = {
+        body: JSON.stringify(response.body),
+      };
+
+      const lambdaParams = {
+        FunctionName: functionName,
+        InvocationType: 'RequestResponse',
+        Payload: JSON.stringify(payload),
+      };
+
+      try {
+        const lambdaResponse = await lambda.invoke(lambdaParams).promise();
+        let valuedata = JSON.parse(lambdaResponse.Payload).body;
+        aggregatedData.push(JSON.parse(valuedata).processed_images_urls);
+      } catch (error) {
+        console.error('Error invoking Lambda function:', error);
+      }
+    }
+
+    res.json(aggregatedData);
+  } catch (error) {
+    console.error(`Error uploading image: ${error}`);
+    res.status(500).json({ success: false, error: 'Internal Lambda Error' });
+  }
 });
 
 app.listen(port, () => {
